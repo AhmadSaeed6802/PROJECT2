8f7b0858f266a660412cbe9fd0a88dabf7628f89
AhmadSaeed6802 <139094382+AhmadSaeed6802@users.noreply.github.com>
2023-07-17 04:16:49 -0700

    Add files via upload

---
+++ project 2 task 2 v4 Bank Management system/project 2  task2 v4 l/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+namespace BankAccountManagementSystem
+    {
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Create a bank
+                Bank bank = new Bank();
+
+                // Create and add bank accounts
+                SavingsAccount savingsAccount = new SavingsAccount("SA001", "Baber");
+                bank.AddAccount(savingsAccount);
+
+                CheckingAccount checkingAccount = new CheckingAccount("CA001", "Hamza ali");
+                bank.AddAccount(checkingAccount);
+
+                LoanAccount loanAccount = new LoanAccount("LA001", "Ali");
+                bank.AddAccount(loanAccount);
+
+                // Perform transactions
+                savingsAccount.ExecuteTransaction(1000);
+                savingsAccount.ExecuteTransaction(500);
+
+                checkingAccount.ExecuteTransaction(2000);
+                checkingAccount.ExecuteTransaction(1000);
+
+                loanAccount.ExecuteTransaction(5000);
+
+                // Display account information and transaction history
+                savingsAccount.DisplayAccountInfo();
+                savingsAccount.DisplayTransactionHistory();
+
+                checkingAccount.DisplayAccountInfo();
+                checkingAccount.DisplayTransactionHistory();
+
+                loanAccount.DisplayAccountInfo();
+                loanAccount.DisplayTransactionHistory();
+
+                Console.ReadLine();
+            }
+        }
+    }
+
